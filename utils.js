e13c6328ea6dab846674e9e326a3649c4f285d55
Nhi <nhicoi2708@gmail.com>
2026-07-08 10:17:28 +0700

    Update tunnel url

--- utils.js
+++ utils.js
@@ -20,7 +20,7 @@ import { db } from "./firebase-config.js";
 
     Lưu ý: không thêm dấu / ở cuối link.
 */
-export const SERVER_BASE_URL = "https://recipe-tee-removing-docs.trycloudflare.com";
+export const SERVER_BASE_URL = "https://seven-higher-gamma-equipment.trycloudflare.com";
 
 export function safe(text) {
     return String(text ?? "")
